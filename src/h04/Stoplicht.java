9dd05872e43e51d10933c54330e06e3cd843f082
Fayaaz Nabie <fnabie@roc-dev.com>
2018-01-15 11:23:44 +0100

    Stoplicht

---
+++ src/h04/Stoplicht.java
@@ -0,0 +1,25 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Stoplicht extends Applet {
+ public void init(){
+     }
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.setColor(Color.gray);
+        g.drawRoundRect(150, 50, 50, 100, 10, 10);
+        g.fillRoundRect(150, 50, 50, 100, 10, 10);
+        g.setColor(Color.RED);
+        g.fillOval(160, 50, 30, 30);
+        g.drawOval(160, 50, 30, 30);
+        g.setColor(Color.YELLOW);
+        g.fillOval(160, 85, 30, 30);
+        g.drawOval(160, 85, 30, 30);
+        g.setColor(Color.GREEN);
+        g.fillOval(160, 120, 30, 30);
+        g.drawOval(160, 120, 30, 30);
+    }
+}
+
