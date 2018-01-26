d3c3c9a4ae8f95f3a014022d56768ae54bfceffc
Fayaaz Nabie <fnabie@roc-dev.com>
2018-01-26 12:37:56 +0100

    alle tot h06

--- src/h06/Gemiddelde.java
+++ src/h06/Gemiddelde.java
@@ -5,23 +5,27 @@ import java.applet.*;
 
 public class Gemiddelde extends Applet {
 
-    double a, b, c, d, uitkomst;
+    double a, b, c, d, uitkomstA, uitkomstR;
 
     public void init(){
 
         a = 5.9;
         b = 6.3;
         c = 6.9;
+        d = 3;
 
+        uitkomstR = (a + b + c)/d;
 
-         uitkomst = (a + b + c) ;
-         uitkomst = (int)(uitkomst)/3;
-         uitkomst = (uitkomst)*10;
-         uitkomst = (uitkomst)/10;
+        uitkomstA = (a + b + c) ;
+         uitkomstA = (uitkomstA/3);
+         uitkomstA = (int)(uitkomstA*10);
+         uitkomstA = uitkomstA/10;
 
     }
 
     public void paint (Graphics g){
-        g.drawString("De uitkomst is:" + uitkomst, 20, 20);
+        g.drawString("Behaalde cijfers: 5.9 - 6.3 - 6.9", 20, 10);
+        g.drawString("De afgeronde uitkomst is:" + uitkomstA, 20, 30);
+        g.drawString("De ruwe uitkomst is: " + uitkomstR, 20,50);
     }
 }
