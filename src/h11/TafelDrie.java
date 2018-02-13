8fce367f30c4abc81da5fa42ebd310a26a1c3d08
Fayaaz Nabie <fnabie@roc-dev.com>
2018-02-13 18:17:18 +0100

    t/m h11

---
+++ src/h11/TafelDrie.java
@@ -0,0 +1,17 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class TafelDrie extends Applet {
+
+    public void paint(Graphics g) {
+        int y = 10;
+
+        for(int a=0; a <= 10; a++) {
+            y += 20;
+            g.drawString(a + " x 3 = " + a * 3, 20, y);
+
+        }
+    }
+}
