5ab59ba751fd3ed3969dc9fa02e09910bd870fcd
Fayaaz Nabie <fnabie@roc-dev.com>
2018-02-15 14:02:58 +0100

    t/m h11

--- src/h11/Cirkels100.java
+++ src/h11/Cirkels100.java
@@ -3,7 +3,7 @@ package h11;
 import java.applet.Applet;
 import java.awt.*;
 
-public class Cirkels extends Applet {
+public class Cirkels100 extends Applet {
 
     public void paint(Graphics g) {
 
