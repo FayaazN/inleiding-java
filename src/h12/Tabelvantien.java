45050dd32f225c47a28ddd6378cb12753c05b361
fayaazn <fnabie@roc-dev.com>
2018-03-15 08:50:28 +0100

    h12

---
+++ src/h12/Tabelvantien.java
@@ -0,0 +1,22 @@
+package H12;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Tabelvantien extends Applet {
+    private double getallen[];
+
+    public void init() {
+        getallen = new double [10];
+
+        for (int teller = 0; teller < getallen.length; teller ++) {
+            getallen[teller] = 100 * teller + 100;
+        }
+    }
+
+    public void paint(Graphics g) {
+        for (int teller = 0; teller < getallen.length; teller ++) {
+            g.drawString("" + getallen[teller], 50, 20 * teller + 20);
+        }
+    }
+}
